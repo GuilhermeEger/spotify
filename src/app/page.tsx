bb04fe590b527048a8a7d7a710ec205b2d7c888e
Guilherme Eger <guilhermeeger.developer@gmail.com>
2024-06-13 14:33:04 +1000

    Adding font family

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import MainPage from "@/components/pages/MainPage/MainPage";
+import { Signika } from 'next/font/google'
+
+const signika = Signika({ subsets: ['latin'], weight: '300' });
 
 export default function Home() {
+
   return (
-    <>
+    <main className={signika.className}>
       <MainPage />
-    </>
+    </main>
   );
 }
